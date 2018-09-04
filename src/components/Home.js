21dd7ad5749949a28fffd8afecd920f7eba98d44
Michael Perkins <mikeperkinsmdp@gmail.com>
2018-09-04 16:21:28 -0600

    updated format

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 
 class Home extends Component {
@@ -7,55 +8,54 @@ class Home extends Component {
         return (
             <div className="page">
                 <div className="home">
-                    <header className="header">
-                        <div className="">
+                    <section id='top'>
+                        <header className="header">
+                            <div className="">
                                 <div className="links">
-                                    <a href="/" className="home-link" id=""><i className="fa fa-home"></i></a>
-                                    <a href="http://localhost:3000#projects">Projects</a>
-                                    <a href="http://localhost:3000#about">About</a>
-                                    <a href="http://localhost:3000#contact">Contact</a>
+                                    <AnchorLink href="#top"><i className="fa fa-home"></i></AnchorLink>
+                                    <AnchorLink offset='60' href="#projects">Projects</AnchorLink>
+                                    <AnchorLink offset='60' href="#about">About</AnchorLink>
+                                    <AnchorLink offset='60' href="#contact">Contact</AnchorLink>
                                 </div>
-                        </div>
-                    </header>
-                    <section className="section1">
-                        <main>
-                                <br/>
-                                <section className="">
-                                    <h1 className="title">Michael Perkins</h1>
-                                    <h2 className="subtitle">Full-Stack Web Development</h2>
-                                </section>
-                                <i className="arrow fa fa-angle-down"></i>
-                        </main>
+                            </div>
+                        </header>
                     </section>
+                    <main>
+                        <br/>
+                        <h1 className="title">Michael <br/> Perkins</h1>
+                        <h2 className="subtitle">Full-Stack <br/> Web Development</h2>
+                        <div className="arrow">
+                            <AnchorLink offset='60' href="#projects" className="arrow"><i className="fa fa-angle-down fa-2x"></i></AnchorLink>
+                        </div>
+                    </main>
                 </div>
-                <a name="projects"></a>
                 <hr className="col-6 hr"/>
                 <div className="projects">
-                    <section className="section2">
+                    <section id='projects'>
                         <h2 className="headers">Projects</h2>
                     </section>
                 </div>
                 <div className="about">
-                    <a name="about"></a>
                     <hr className="col-6 hr"/>
                     <section className="section3">
-                        <h2 className="headers">About</h2>
+                        <section id='about'>
+                            <h2 className="headers">About</h2>
+                        </section>
                         <h4>Hi, I am a full-stack developer from Salt Lake City, Utah.</h4>
                     </section>
                 </div>
-                <a name="contact"></a>
                 <hr className="col-6 hr"/>
                 <div className="contact">
-                    <section className="section4">
+                    <section id='contact'>
                         <h2 className="headers">Contact</h2>
-                        <h3>Feel free to reach out to me</h3>
-                        <br/>
-                        <div className="contacts">
-                            <h5>Email: michael.perkins.d@gmail.com</h5>
-                            <br/>
-                            <h5>LinkedIn: <a className="linked-in" href="https://www.linkedin.com/in/michael-perkins-d/">Michael Perkins</a></h5>
-                        </div>
                     </section>
+                    <h3>Feel free to reach out to me</h3>
+                    <br/>
+                    <div className="contacts">
+                        <h5>Email: michael.perkins.d@gmail.com</h5>
+                        <br/>
+                        <h5>LinkedIn: <a className="linked-in" href="https://www.linkedin.com/in/michael-perkins-d/">Michael Perkins</a></h5>
+                    </div>
                 </div>
                 <hr className="col-6 hr"/>
                 <div className="container-fluid social">
