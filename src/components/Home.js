c62ac9876a8d5b3a225289ee64bed718fc41ab3b
Michael Perkins <mikeperkinsmdp@gmail.com>
2018-09-05 17:26:00 -0600

    updated ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,9 +11,9 @@ class Home extends Component {
                     <section id='top'>
                         <header className="header">
                             <AnchorLink href="#top"><i className="fa fa-home"></i></AnchorLink>
-                            <AnchorLink offset='60' href="#projects">Projects</AnchorLink>
-                            <AnchorLink offset='60' href="#about">About</AnchorLink>
-                            <AnchorLink offset='60' href="#contact">Contact</AnchorLink>
+                            <AnchorLink offset='-600' href="#projects">Projects</AnchorLink>
+                            <AnchorLink offset='-1236' href="#about">About</AnchorLink>
+                            <AnchorLink offset='-1902' href="#contact">Contact</AnchorLink>
                         </header>
                     </section>
                     <br/>
@@ -22,35 +22,69 @@ class Home extends Component {
                         <h2 className="subtitle col">Full-Stack <br/> Web Development</h2>
                     </div>
                     <div className="arrow">
-                        <AnchorLink offset='60' href="#projects" className="arrow"><i className="fa fa-angle-down fa-2x"></i></AnchorLink>
+                        <AnchorLink offset='-600' href="#projects" className="arrow"><i className="fa fa-angle-down fa-2x"></i></AnchorLink>
                     </div>
                 </div>
-                <hr className="col-6 hr"/>
-                <div className="projects">
-                    <section id='projects'>
-                        <h2 className="headers">Projects</h2>
-                    </section>
-                </div>
-                <div className="about">
-                    <hr className="col-6 hr"/>
-                    <section>
-                        <section id='about'>
-                            <h2 className="headers">About</h2>
-                        </section>
-                        <h4>Hi, I am a full-stack developer from Salt Lake City, Utah.</h4>
-                    </section>
-                </div>
-                <hr className="col-6 hr"/>
-                <div className="contact">
-                    <section id='contact'>
-                        <h2 className="headers">Contact</h2>
-                    </section>
-                    <h3>Feel free to reach out to me</h3>
+                <div className="p-parallax">
                     <br/>
-                    <div className="contacts">
-                        <h5>Email: michael.perkins.d@gmail.com</h5>
-                        <br/>
-                        <h5>LinkedIn: <a className="linked-in" href="https://www.linkedin.com/in/michael-perkins-d/">Michael Perkins</a></h5>
+                    <div className="projects">
+                        <section id='projects' className="row justify-content-center">
+                            <h2 className="headers col-2">Projects</h2>
+                        </section>
+                        <hr className="col-6 hr"/>
+                        <div className="project-content container">
+                            <p>Here are my projects</p>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>
+                    </div>
+                </div>
+                <div className="break"></div>
+                <div className="a-parallax">
+                    <br/>
+                    <div className="about">
+                        <section id='about' className="row justify-content-center">
+                            <h2 className="headers col-2">About</h2>
+                        </section>
+                        <hr className="col-6 hr"/>
+                        <div className="about-content container">
+                            <p>Hi, I am a full-stack developer from Salt Lake City, Utah.</p>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                            <br/>
+                        </div>
+                    </div>
+                </div>
+                <div className="break"></div>
+                <div className="c-parallax">
+                    <br/>
+                    <div className="contact">
+                        <section id='contact' className="row justify-content-center">
+                            <h2 className="headers col-2">Contact</h2>
+                        </section>
+                        <hr className="col-6 hr"/>
+                        <div className="contact-content container">
+                            <p>Feel free to reach out to me</p>
+                            <div className="contacts">
+                                <p>Email: michael.perkins.d@gmail.com</p>
+                            <p>LinkedIn: <a className="linked-in" href="https://www.linkedin.com/in/michael-perkins-d/">Michael Perkins</a></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <hr className="col-6 hr"/>
